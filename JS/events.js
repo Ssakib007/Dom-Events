c2d70a2cc1a2f83e7034bb359a446410d01ae7ae
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-29 01:27:52 +0600

    event listener

--- JS/events.js
+++ JS/events.js
@@ -1,1 +1,37 @@
-console.log("external file");
+// console.log("external file");
+
+// option 2 to handle events -- by calling a function
+
+function makeYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+//option 3 to handle events -- get element by id and set onclick
+const btnMakeBlue = document.getElementById("btn-make-blue");
+btnMakeBlue.onclick = function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+};
+
+// option 3 to handle events different version
+const btnMakePurple = document.getElementById("btn-make-purple");
+function makePurple() {
+  document.body.style.backgroundColor = "purple";
+}
+btnMakePurple.onclick = makePurple;
+
+/// option 4 -- add event listener
+//getElementByID.addEventListener
+document
+  .getElementById("btn-make-green")
+  .addEventListener("click", function makeGreen() {
+    document.body.style.backgroundColor = "green";
+  });
+
+document
+  .getElementById("btn-make-gold")
+  .addEventListener("click", function makeGold() {
+    document.body.style.backgroundColor = "gold";
+  });
