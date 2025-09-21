f37233808a89c1a7c512bb0cf66fddee4161676e
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-09-21 22:20:36 +0600

    comment box

--- JS/commentBox.js
+++ JS/commentBox.js
@@ -5,7 +5,7 @@ document
     /// get the text written in the comment text area
     const commentTextBox = document.getElementById("comment-text-box");
 
-    const newComment = commentTextBox.value;
+    // const newComment = commentTextBox.value;
 
     // get the parent node where to publish comment
     const commentContainer = document.getElementById("comment-container");
@@ -13,7 +13,7 @@ document
     //// create a comment paragraphn and set the innertext
      const commentElement = document.createElement("p");
     commentElement.classList.add("comment");
-    commentElement.innerText = newComment;
+    commentElement.innerText = commentTextBox.value;
 
     /// append the new p tag to the parent node
     commentContainer.appendChild(commentElement);
